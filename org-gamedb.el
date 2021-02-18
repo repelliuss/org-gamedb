b7174caf9791eedef9209d93c8b249309fd697e6
repelliuss <repelliuss@gmail.com>
2021-02-18 19:33:52 +0300

    Create entry file

---
+++ org-gamedb.el
@@ -0,0 +1,25 @@
+;;; org-gamedb.el --- An GiantBomb API client to work with Emacs org-mode -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2021 repelliuss
+;;
+;; Author: repelliuss <https://github.com/repelliuss>
+;; Maintainer: repelliuss <repelliuss@gmail.com>
+;; Created: February 17, 2021
+;; Modified: February 17, 2021
+;; Version: 0.0.1
+;; Keywords: Symbol’s value as variable is void: finder-known-keywords
+;; Homepage: https://github.com/repelliuss/org-gamedb
+;; Package-Requires: ((emacs "24.3"))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;;
+;;  An GiantBomb API client to work with Emacs org-mod
+;;
+;;; Code:
+
+
+
+(provide 'org-gamedb)
+;;; org-gamedb.el ends here
